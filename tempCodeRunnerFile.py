9d2a893d051c565846eab3edb50c37114e22d11b
= <luigi.naudi@gmail.com>
2023-12-09 22:47:25 +0100

    Very Close 90% accuracy

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,1 @@
-def cross_entropy_loss(y_true, y_pred):
-    y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
-    return -np.sum(y_true * np.log(y_pred)) / len(y_true)
+        print("Output",output_layer_output)

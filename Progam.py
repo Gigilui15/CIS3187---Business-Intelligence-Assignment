ba38bd2613d25c1b57e05ab9c0e42f8906ff967e
= <luigi.naudi@gmail.com>
2023-12-10 19:05:53 +0100

    Model is 100% accurate. Logging and more printing detail to add

--- Progam.py
+++ Progam.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import pickle
+import os
 
 # Loading the dataset with no headers
 dataset = pd.read_csv('C:\\Users\\luigi\\Desktop\\Third Year\\Business Intelligence\\IrisDataset.csv')
@@ -16,21 +18,72 @@ training_data = pd.read_csv('C:\\Users\\luigi\\Desktop\\Third Year\\Business Int
 # Loading the testing dataset
 testing_data = pd.read_csv('C:\\Users\\luigi\\Desktop\\Third Year\\Business Intelligence\\testing20.csv')
 
-# Softmax function for the output layer
-def softmax(x):
-    exp_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
-    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+#Creating a folder to save weights
+weights_folder = 'Weights'
+os.makedirs(weights_folder, exist_ok=True)
 
 # Sigmoid function for the hidden and output layer neurons
 def sigmoid(x):
-    x = np.clip(x, -500, 500)
     return 1 / (1 + np.exp(-x))
 
 # Sigmoid derivative to be used in Error Backpropagation formula: error * sigmoid_derivative
 def sigmoid_derivative(x):
     return x * (1 - x)
 
-#loading the training inputs (120x4)
+def softmax(x):
+    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+
+def forward_propagation(fit, input_weights, hidden_weights, dropout_rate):
+    hidden_layer_input = np.dot(fit, input_weights)
+    hidden_layer_output = sigmoid(hidden_layer_input)
+    
+    # Dropout in the hidden layer
+    hidden_layer_output = dropout(hidden_layer_output, dropout_rate)
+
+    output_layer_input = np.dot(hidden_layer_output, hidden_weights)
+    output_layer_output = softmax(output_layer_input)
+    return hidden_layer_output, output_layer_output
+
+def backpropagation(inputs, input_weights, hidden_weights, output_layer_output, outputs, hidden_layer_output, dropout_rate):
+    output_error = outputs - output_layer_output
+    output_delta = output_error * sigmoid_derivative(output_layer_output)
+
+    hidden_error = output_delta.dot(hidden_weights.T)
+    hidden_delta = hidden_error * sigmoid_derivative(hidden_layer_output)
+
+    hidden_weights += hidden_layer_output.T.dot(output_delta) * learning_rate
+    input_weights += inputs.T.dot(hidden_delta) * learning_rate
+
+    return hidden_weights, input_weights
+
+def dropout(x, dropout_rate):
+    if dropout_rate > 0.0:
+        mask = (np.random.rand(*x.shape) < dropout_rate) / (1 - dropout_rate)
+        return x * mask
+    else:
+        return x
+
+def training_Accuracy(epoch, bad_facts_count, total_examples):
+    accuracy = ((total_examples - bad_facts_count) / total_examples) * 100
+    print(f"\rTraining the Moodel | Epoch: {epoch + 1} | Bad Facts: {bad_facts_count} | Accuracy: {accuracy:.2f}%", end="", flush=True)
+    
+def testing_Accuracy(inputs, outputs, input_weights, hidden_weights):
+    correct_predictions = 0
+
+    for example in range(len(inputs)):
+        fit = inputs[example].reshape(1, -1)
+        _, output_layer_output = forward_propagation(fit, input_weights, hidden_weights, dropout_rate)
+        predicted_class = np.argmax(output_layer_output)
+        true_class = np.argmax(outputs[example])
+
+        if predicted_class == true_class:
+            correct_predictions += 1
+
+    accuracy = (correct_predictions / len(inputs)) * 100
+    print(f"\nTesting Accuracy: {accuracy:.2f}%")
+    
+#loading the training inputs
 inputs = training_data.iloc[:, :-3].values
 #loading the training answers
 outputs = training_data.iloc[:, -3:].values
@@ -38,76 +91,45 @@ outputs = training_data.iloc[:, -3:].values
 #Hyperparameters
 learning_rate = 0.2
 error_threshold = 0.2
-epochs = 10000
+epochs = 3000
 input_layer_size = 4
 hidden_layer_size = 4
 output_layer_size = 3
+dropout_rate = 0
+seed_range = [1,10,21,23,30, 91]
 
-#Seeding for reproducability
-# Seeding for reproducibility
-np.random.seed(10)
-
-# Generating random weights for the input layer (4x4)
-input_weights = np.random.uniform(size=(input_layer_size, hidden_layer_size))
-# Generating random weights for the hidden layer (4x3)
-hidden_weights = np.random.uniform(size=(hidden_layer_size, output_layer_size))
-
-facts = []
-
-def forward_propagation(inputs, input_weights, hidden_weights):
-    hidden_layer_input = np.dot(inputs, input_weights)
-    hidden_layer_output = sigmoid(hidden_layer_input)
-
-    output_layer_input = np.dot(hidden_layer_output, hidden_weights)
-    output_layer_output = softmax(output_layer_input)
-    return hidden_layer_output, output_layer_output
-
-def backpropagation(inputs, input_weights, hidden_weights, output_layer_output, outputs):
-    delta_output = (output_layer_output - outputs) / len(inputs)
-    hidden_weights += learning_rate * np.dot(hidden_layer_output.T, delta_output)
+for seed in seed_range:
+    print(f"\nUsing seed {seed}:")
     
-    delta_hidden = sigmoid_derivative(hidden_layer_output) * np.dot(delta_output, hidden_weights.T)
-    input_weights += learning_rate * np.dot(inputs.T, delta_hidden)
+    # Seeding for reproducibility
+    np.random.seed(seed)
 
-    return hidden_weights, input_weights
+    # Generating random weights for the input layer (4x4)
+    input_weights = np.random.uniform(size=(input_layer_size, hidden_layer_size))
+    # Generating random weights for the hidden layer (4x3)
+    hidden_weights = np.random.uniform(size=(hidden_layer_size, output_layer_size))
 
-# Loading the training inputs (120x4)
-inputs = training_data.iloc[:, :-3].values
-# Loading the training answers
-outputs = training_data.iloc[:, -3:].values
+    for epoch in range(epochs):
+        bad_facts_count = 0
+        for example in range(len(inputs)):
+            fit = inputs[example].reshape(1, -1)
+            hidden_layer_output, output_layer_output = forward_propagation(fit, input_weights, hidden_weights, dropout_rate)
+            error = outputs[example] - output_layer_output
 
-# Training loop with forward and backward propagation
-for epoch in range(epochs):
-    hidden_layer_output, output_layer_output = forward_propagation(inputs, input_weights, hidden_weights)
+            if any(abs(e) > error_threshold for e in error[0]):
+                hidden_weights, input_weights = backpropagation(fit, input_weights, hidden_weights, output_layer_output, outputs[example], hidden_layer_output, dropout_rate)
+                bad_facts_count += 1
 
-    epoch_facts = []
-    correct_predictions = 0
+        training_Accuracy(epoch, bad_facts_count, len(inputs))
 
-    error = outputs - output_layer_output
+        # Check if bad facts occurred, and break if not
+        if bad_facts_count == 0:
+            print("\nNo bad facts in the last epoch. Stopping training.")
+            print(f"Found seed {seed} with 100% accuracy.")
+            break
+    # Save weights after training
+    with open(os.path.join(weights_folder, f'weights_seed_{seed}.pkl'), 'wb') as file:
+        weights_dict = {'input_weights': input_weights, 'hidden_weights': hidden_weights}
+        pickle.dump(weights_dict, file)
 
-    for i in range(len(inputs)):
-        
-        if np.any(error < error_threshold):
-            status = 'Good Fact'
-        else:
-            status = 'Bad Fact'
-            hidden_weights, input_weights = backpropagation(inputs, input_weights, hidden_weights, output_layer_output, outputs)
-
-        fact = {
-            'Epoch No:': epoch + 1,
-            'Index:': i,
-            'Expected Output:': outputs[i],
-            'Actual Output:': output_layer_output[i],
-            'Error:': error,
-            'Status: ': status
-        }
-        epoch_facts.append(fact)
-
-    accuracy = correct_predictions / len(inputs)
-
-    facts.append(epoch_facts)
-
-facts_df = pd.DataFrame([fact for epoch_facts in facts for fact in epoch_facts])
-facts_df.to_csv('C:\\Users\\luigi\\Desktop\\Third Year\\Business Intelligence\\Model Results\\Allfacts.csv', index=False)
-
-print("Done")
+    testing_Accuracy(testing_data.iloc[:, :-3].values, testing_data.iloc[:, -3:].values, input_weights, hidden_weights)
